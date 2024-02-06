84536c1c0720804dc4747dc690f32560b333c8f9
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-06 22:51:30 +0700

    Add navigation button to appbar

--- app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
+++ app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
@@ -1,18 +1,27 @@
 package mnxk.kotlintex.mywishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
     // A surface container using the 'background' color from the theme
     Scaffold(
         topBar = {
-            AppBarView(title = "My Wish List")
+            AppBarView(title = "My Wish List", {
+                Toast.makeText(
+                    context,
+                    "Back button clicked",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            })
         },
     ) {
         LazyColumn(
