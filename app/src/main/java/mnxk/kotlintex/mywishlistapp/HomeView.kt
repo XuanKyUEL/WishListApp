50f51b0ff70c083f4eb9622bf4fbac3fb286180d
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-06 23:17:31 +0700

    add floating + button

--- app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
+++ app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
@@ -4,10 +4,16 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Icon as Icon
 
 @Composable
 fun HomeView() {
@@ -23,9 +29,28 @@ fun HomeView() {
                 ).show()
             })
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 16.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = { // Todo add navigation to the screen}) {
+                    Toast.makeText(
+                        context,
+                        "Add button clicked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                },
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        },
     ) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(it),
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(it),
         ) {
         }
     }
