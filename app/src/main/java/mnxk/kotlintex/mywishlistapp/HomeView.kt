c7f15ca4042d38ebe532f9fb0013e809b30c9024
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-08 19:23:36 +0700

    add dummy wish

--- app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
+++ app/src/main/java/mnxk/kotlintex/mywishlistapp/HomeView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
@@ -19,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import mnxk.kotlintex.mywishlistapp.data.DummyWish
 import mnxk.kotlintex.mywishlistapp.data.Wish
 import androidx.compose.material.Icon as Icon
 
@@ -59,6 +61,11 @@ fun HomeView() {
                     .fillMaxSize()
                     .padding(it),
         ) {
+            items(DummyWish.wishList) {
+                    wish ->
+                WishItem(wish = wish) {
+                }
+            }
         }
     }
 }
