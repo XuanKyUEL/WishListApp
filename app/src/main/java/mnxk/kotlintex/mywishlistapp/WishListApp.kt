19501cf85a952ae5909f395427f25b2ea0ebc60e
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-10 17:48:59 +0700

    Add application

---
+++ app/src/main/java/mnxk/kotlintex/mywishlistapp/WishListApp.kt
@@ -0,0 +1,10 @@
+package mnxk.kotlintex.mywishlistapp
+
+import android.app.Application
+
+class WishListApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Graph.provide(context = this)
+    }
+}
